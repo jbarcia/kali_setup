eaa4a31cbd5d88306f787350580ec71e7bdaa85b
jbarcia <jbarcia99@yahoo.com>
2015-07-20 10:17:19 -0400

    loki debs

--- kali_setup.sh
+++ kali_setup.sh
@@ -5,6 +5,7 @@ apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade
 
 #Add dependencies
 apt-get remove python-pypcap && apt-get -y install cifs-utils libssh2-1 libssh2-1-dev libgnutls-dev python-libpcap conntrack mingw32 terminator shutter screen tmux python-support libdumbnet1 python-ipy python-glade2
+
 cp -R /usr/share/doc/python-impacket-doc/examples/smbrelayx /toolslinux/exploits/impacket/impacket/
 
 #########################################
@@ -15,24 +16,11 @@ echo "## arrow down" >> ~/.inputrc
 echo "\"\\e[B\":history-search-forward" >> ~/.inputrc
 #########################################
 
-./github_clone.sh
-
 #########################################
 # Loki Setup
-apt-get remove python-libpcap
+apt-get -y remove python-libpcap
 
-mkdir -p ~/Loki
-cd ~/Loki
-wget https://www.ernw.de/wp-content/uploads/pylibpcap_0.6.2-1_i386.deb
-wget http://snapshot.debian.org/archive/debian/20110406T213352Z/pool/main/o/openssl098/libssl0.9.8_0.9.8o-7_i386.deb
-wget http://ftp.us.debian.org/debian/pool/main/p/python-dpkt/python-dpkt_1.6+svn54-1_all.deb
-wget http://mirrors.kernel.org/ubuntu/pool/universe/libd/libdumbnet/python-dumbnet_1.12-3.1_i386.deb
-wget http://launchpadlibrarian.net/103839793/python-central_0.6.17ubuntu2_all.deb
-wget https://www.ernw.de/wp-content/uploads/loki_0.2.7-1_i386.deb
-wget https://www.ernw.de/wp-content/uploads/pylibpcap_0.6.2-1_amd64.deb
-wget http://snapshot.debian.org/archive/debian/20110406T213352Z/pool/main/o/openssl098/libssl0.9.8_0.9.8o-7_amd64.deb
-wget http://mirrors.kernel.org/ubuntu/pool/universe/libd/libdumbnet/python-dumbnet_1.12-3.1_amd64.deb
-wget https://www.ernw.de/wp-content/uploads/loki_0.2.7-1_amd64.deb
+cd loki_debs
 
 dpkg -i python-central_0.6.17ubuntu2_all.deb
 
@@ -41,6 +29,18 @@ dpkg -i libssl0.9.8_0.9.8o-7_i386.deb python-dpkt_1.6+svn54-1_all.deb python-dum
 dpkg –i loki_0.2.7-1_i386.deb
 
 # x64
-dpkg -i libssl0.9.8_0.9.8o-7_amd64.deb python-dpkt_1.6+svn54-1_all.deb python-dumbnet_1.12-3.1_amd64.deb pylibpcap_0.6.2-1_amd64.deb
+dpkg -i libssl0.9.8_0.9.8o-7_amd64.deb python-dpkt_1.6+svn54-1_all.deb python-dumbnet_1.12-3.1_amd64.deb pylibpcap_0.6.2-1_amd64.deb libcap-dev_2.22-1.2_amd64.deb libpcap0.8-dev_1.3.0-1_amd64.deb
 dpkg -i loki_0.2.7-1_amd64.deb
+#########################################
+
+cd ..
+./github_clone.sh
+
+#########################################
+# Crowe Medusa v2.2_rc2
+cp /toolslinux/passwords/medusa/Crowe_Medusa-2.2_rc2.zip /tmp/
+cd /tmp
+unzip Crowe_Medusa-2.2_rc2.zip
+cd root/medusa-2.2_rc2/
+./configure && make && make install
 #########################################
